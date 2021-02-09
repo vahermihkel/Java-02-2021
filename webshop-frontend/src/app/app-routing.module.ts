f97578f21a75284222013e14ddae0d3422649139
vahermihkel <vaher.mihkel@gmail.com>
2021-02-09 20:08:10 +0200

    routes

--- webshop-frontend/src/app/app-routing.module.ts
+++ webshop-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemAddComponent } from './item/item-add/item-add.component';
+import { ItemEditComponent } from './item/item-edit/item-edit.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
+import { ItemViewComponent } from './item/item-view/item-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: ItemListComponent },
+  { path: "item/edit", component: ItemEditComponent },
+  { path: "item/view", component: ItemViewComponent },
+  { path: "item/add", component: ItemAddComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
